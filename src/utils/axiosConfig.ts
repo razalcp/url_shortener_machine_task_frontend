f86fa141dd284eed90de8cc177df1c5dc964d0b0
razalcp <cprazalnazim@gmail.com>
2025-07-10 13:54:09 +0530

    vercel proxy added

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -1,10 +1,16 @@
 import axios from 'axios'
 
-console.log("url",process.env.REACT_APP_USER_API_BASE_URL);
+// console.log("url",process.env.REACT_APP_USER_API_BASE_URL);
+
+// const userApi = axios.create({
+//     baseURL: process.env.REACT_APP_USER_API_BASE_URL,
+//     withCredentials: true
+// })
+
+// export default userApi
 
 const userApi = axios.create({
-    baseURL: process.env.REACT_APP_USER_API_BASE_URL,
+    baseURL: "/api", // now hitting your local Vercel proxy
     withCredentials: true
-})
-
+});
 export default userApi
